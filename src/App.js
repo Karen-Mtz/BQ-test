ef9b52ecf1f32a5a2f140f8e5350745a5d749bb8
karen-mtz <dkareen.mtz@gmail.com>
2019-05-17 15:13:21 -0500

    integrando maqueta css y componentes de alimentos y detalle de orden

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import Home from "./Home";
 import Login from "./Login";
 import { AuthProvider } from "./Auth";
 import PrivateRoute from "./PrivateRoute";
+import Navbar from './Navbar';
+import Details from './Details';
+import Meals from './Meals';
+import Drinks from './Drinks';
+import Complements from './Complements';
+import Breakfastfood from './Breakfastfood';
+import Menu from './Menu';
+import BreakfastDrinks from './BreakfastDrinks';
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <div>
+        <div className="App">
+        <Navbar/>
+        <Details/>
           <PrivateRoute exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/meals" component={Meals} />
+          <Route exact path="/breakfastfood" component={Breakfastfood} />
+          <Route exact path="/breakfastdrinks" component={BreakfastDrinks} />
+          <Route exact path="/drinks" component={Drinks} />
+          <Route exact path="/complements" component={Complements} />
         </div>
       </Router>
     </AuthProvider>
@@ -29,14 +44,14 @@ export default App;
 // import {AuthProvider} from './components/Auth';
 // import PrivateRoute from './components/PrivateRoute';
 
-// // import Navbar from './components/Navbar';
-// // import Details from './components/Details';
-// // import Meals from './components/Meals';
-// // import Drinks from './components/Drinks';
-// // import Complements from './components/Complements';
-// // import Breakfastfood from './components/Breakfastfood';
-// // import Menu from './components/Menu';
-// // import BreakfastDrinks from './components/BreakfastDrinks';
+// import Navbar from './components/Navbar';
+// import Details from './components/Details';
+// import Meals from './components/Meals';
+// import Drinks from './components/Drinks';
+// import Complements from './components/Complements';
+// import Breakfastfood from './components/Breakfastfood';
+// import Menu from './components/Menu';
+// import BreakfastDrinks from './components/BreakfastDrinks';
 
 // //BQ
 
@@ -49,12 +64,12 @@ export default App;
 // //     <Login />
 // //     <Menu/>
 // //     <Details/>
-// //     <Route exact path="/meals" component={Meals}/>
+    // <Route exact path="/meals" component={Meals}/>
 
-// //     <Route exact path="/breakfastfood" component={Breakfastfood}/>
-// //     <Route exact path="/breakfastdrinks" component={BreakfastDrinks}/>
-// //     <Route exact path="/drinks" component={Drinks}/>
-// //     <Route exact path="/complements" component={Complements}/>
+    // <Route exact path="/breakfastfood" component={Breakfastfood}/>
+    // <Route exact path="/breakfastdrinks" component={BreakfastDrinks}/>
+    // <Route exact path="/drinks" component={Drinks}/>
+    // <Route exact path="/complements" component={Complements}/>
 // //     </div>
 // //     </BrowserRouter>
 // //   );
