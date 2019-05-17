c025b4aa68dd9f9ef525dde57a04312011173364
Aldo Rodríguez <aldo@overcode.mx>
2019-05-17 11:11:44 -0500

    firebase auth loading state

--- src/Auth.js
+++ src/Auth.js
@@ -5,15 +5,19 @@ export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    app.auth().onAuthStateChanged(setCurrentUser);
+    app.auth().onAuthStateChanged(user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
   }, []);
 
   return (
     <AuthContext.Provider
       value={{
-        currentUser
+        currentUser, loading
       }}
     >
       {children}
