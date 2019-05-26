9ac4b4fc045f9184d6d7616679d007a306d229f8
karen-mtz <dkareen.mtz@gmail.com>
2019-05-26 04:25:43 -0500

    en consola precio de productos

--- src/Details.js
+++ src/Details.js
@@ -3,10 +3,22 @@ import React from 'react';
 
 
 export default class Details extends React.Component {
+    state = {
+        total: 0
+    }
+
+    changeTotal = price => {
+      
+    console.log(this.props.ticketElements.map(element => (
+        element.price
+    )))
+    }
     
     render() {
         return (
+           
             <section className="Ticket-details">
+                 {this.changeTotal()}
                 <div>
         <h3 className="Ticket-details-h">Cantidad    Descripción    Monto</h3>
         </div>
@@ -15,7 +27,7 @@ export default class Details extends React.Component {
             <div key={element.id}>
              <h3 className="Ticket-details-h"> {element.item}    $ {element.price}</h3>
            </div>))}
-
+<h1>TOTAL:  </h1>
         
 </section>
         );
